f90000db26a85d9719adec99d0e272d589d4f10e
Gorshenina Jullia <gorshenina@smartway.today>
2019-10-02 18:27:04 +0300

    Add new page and test

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from pages.locators import MainPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url
+
+    def should_be_login_form(self):
+        self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not present"
+        assert True
+
+    def should_be_register_form(self):
+        self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register form is not present"
+        assert True
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -6,6 +7,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page_check = LoginPage(browser, link)
+    page_check.should_be_login_url()
+    page_check.should_be_login_form()
+    page_check.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
